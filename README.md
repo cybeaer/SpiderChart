778eb8c552198698d01f9f28e0dbce74b0d74270
cybeaer <attenberger@gmail.com>
2022-05-30 21:33:11 +0200

    added entity and legend render

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # SpiderChart
 
-## Implement
+A minimalistic radar (spider) chart script displaying 5 values in a pentagon.
+
+# Implement
+
+## shortest way without values
 ```html
 <canvas id="chart"></canvas>
 <script src="SpiderChart.js"></script>
@@ -11,4 +15,68 @@
 ```
 result:
 
-![img.png](img.png)
+![img.png](short_way.png)
+
+## all possible options
+### initializing
+```javascript
+let sc = new SpiderChart('chart',250,250);
+```
+parameters:
+- id,
+- height
+- width
+
+### entities
+```javascript
+let entity_one = {
+            "id": 'test1',
+            "data": [
+                50,
+                40,
+                60,
+                50,
+                70
+            ],
+            "color": 'rgba(255, 178, 102, 0.3)'
+        }
+sc.addEntity(entity_one);
+```
+- an entity requires an id, data and color attribute.
+- data requires an array with 5 int values, that represent a percent value
+
+### offset
+```javascript
+sc.setOffset(1.05);
+```
+int value that moves the y center
+
+### grid display
+```javascript
+sc.setGrid('lightgray', 1, 0.5);
+```
+parameters:
+- color
+- strike width
+- opacity
+
+### corner captions
+```javascript
+sc.setParams(['aa','bb','cc','dd','ee']);
+```
+array with 5 strings
+
+### render graph
+```javascript
+sc.renderGraph()
+```
+renders pentagram chart with given options
+
+### render legend
+```javascript
+sc.renderLegend();
+```
+renders list of first 5 entity id's at the bottom.
+
+### result
+![img_1.png](long_way.png)
--- SpiderChart.js
+++ SpiderChart.js
@@ -1,23 +1,67 @@
 'use strict'
 
+/**
+ *
+ */
 class SpiderChart{
+    /**
+     * @type {HTMLElement}
+     * @private
+     */
     _canvas = null;
+    /**
+     * @type {context}
+     * @private
+     */
     _ctx = null;
+    /**
+     * @type {{x: number, y: number}}
+     * @private
+     */
     _center = {};
+    /**
+     * @type {array}
+     * @private
+     */
     _points = [];
+    /**
+     * @type {array}
+     * @private
+     */
     _entities = [];
+    /**
+     * @type {number}
+     * @private
+     */
+    _shrink = 0;
+    /**
+     * @type {int}
+     * @private
+     */
+    _offset = 1.05;
+    /**
+     * @type {{strikeColor: string, strikeWidth: int, strikeOpacity: int, params: string[]}}
+     * @private
+     */
     _options = {
-        'strikeColor': 'lightgray',//'#000000',
+        'strikeColor': 'lightgray',
         'strikeWidth': 1,
-        'strikeOpac': 0.5,
+        'strikeOpacity': 0.5,
         'params': [
-            'LIFE',
-            'INT',
-            'STR',
-            'AGI',
-            'HP'
+            '1',
+            '2',
+            '3',
+            '4',
+            '5'
         ]
     };
+
+    /**
+     * initialize class
+     * @param {string} canvasId - canvas tag id
+     * @param {int} height - canvas height
+     * @param {int} width - canvas width
+     */
     constructor(canvasId, height, width){
         this._canvas = document.getElementById(canvasId);
         this._canvas.height=height;
@@ -25,54 +69,126 @@ class SpiderChart{
         this._ctx = this._canvas.getContext('2d');
     }
 
-    addEntity(data){
-        if('undefined' == data['id'] || 'undefined' == data['data'])
-            return false;
-        this._entities[data['id']] = data;
+    /**
+     * sets the center offset of the pentagon
+     * @param {int} offset - offset of the y coordinates
+     */
+    setOffset(offset){
+        this._offset = offset;
     }
 
-    renderGraph(interval, size=null, recursiv=false){
+    /**
+     * sets color, strike width and opacity of chart
+     * @param {string} color - html color code starting with #
+     * @param {int} width - line width
+     * @param {int} opacity - opacity of grid
+     */
+    setGrid(color,width,opacity){
+        this._options.strikeColor = color;
+        this._options.strikeWidth = width;
+        this._options.strikeOpacity = opacity
+    }
 
+    /**
+     * set corner names (5)
+     * @param {array} params - array containing 5 corner captions
+     */
+    setParams(params){
+        this._options.params = params;
+    }
+
+    /**
+     * add entity (only first 5 will be displayed in legend)
+     * @param {object} data - object containing with id, data (array with 5 values) and color
+     * @returns {boolean} - false if object data invalid
+     */
+    addEntity(data){
+        if(undefined === data['id'] || undefined === data['data'])
+            return false;
+        this._entities.push(data);
+        return true;
+    }
+
+    /**
+     * renders the pentagon graph
+     * @param {int} interval - count of inner "spiderwebs"
+     * @param {int} size - size of pentagon - default width/2
+     * @param {boolean} recursive - true for last inner spiderweb
+     */
+    renderGraph(interval=5, size=null, recursive=false){
         this._center.x = this._canvas.width / 2;
         this._center.y = this._canvas.height / 2;
         if(size===null)
             size = this._center.x*0.9
+        if(this._shrink === 0)
+            this._shrink = size/ interval;
         let numberOfSides = 5,
-            Xcenter = this._center.x,
-            Ycenter = this._center.y*1.1,
             step  = 2 * Math.PI / numberOfSides,
             shift = (Math.PI / 180.0) * -18;
-
         this._ctx.beginPath();
         for (let i = 0; i <= numberOfSides;i++) {
             let curStep = i * step + shift;
-            let x = Xcenter + size * Math.cos(curStep);
-            let y = Ycenter + size * Math.sin(curStep);
+            let x = this._center.x + size * Math.cos(curStep);
+            let y = this._center.y*this._offset + size * Math.sin(curStep);
             this._ctx.lineTo (x,y);
-            if(!recursiv && i != numberOfSides) {
+            if(!recursive && i != numberOfSides) {
                 this._ctx.fillText(this._options.params[i], x - 10, y);
                 this._points.push({'x':x, 'y':y});
             }
         }
-
         this._ctx.strokeStyle = this._options.strikeColor;
         this._ctx.lineWidth = this._options.strikeWidth;
         this._ctx.stroke();
-
         if(interval-1 > 0)
-            this.renderGraph(interval-1,size-20, true)
+            this.renderGraph(interval-1,size-this._shrink, true)
         else {
-            console.log(this._points)
+            this._shrink = 0;
             this._points.forEach(point => {
-                console.log(point)
                 this._ctx.beginPath();
-                this._ctx.moveTo(this._center.x, this._center.y*1.1);
+                this._ctx.moveTo(this._center.x, this._center.y*this._offset);
                 this._ctx.lineTo(point.x, point.y);
 
                 this._ctx.strokeStyle = this._options.strikeColor;
                 this._ctx.lineWidth = this._options.strikeWidth;
                 this._ctx.stroke();
             });
+            this.renderEntities();
         }
+    }
+
+    /**
+     * renders the entities above the graph
+     */
+    renderEntities(){
+        this._entities.forEach(entity=>{
+            this._ctx.beginPath();
+            this._points.forEach((point, idx) => {
+                let multi = entity.data[idx]/100;
+                let vector = [(point.x-this._center.x)*multi,(point.y-this._center.y*this._offset)*multi];
+                this._ctx.lineTo(this._center.x+vector[0], this._center.y*this._offset+vector[1]);
+                this._ctx.strokeStyle = this._options.strikeColor;
+                this._ctx.lineWidth = this._options.strikeWidth;
+                this._ctx.stroke();
+            });
+            this._ctx.closePath();
+            this._ctx.fillStyle = entity.color;//"rgba(0, 0, 0, 0.5)";
+            this._ctx.fill();
+        });
+    }
+
+    /**
+     * renders legend of the first 5 entities at the bottom
+     */
+    renderLegend(){
+        let bottom = this._canvas.height -10;
+        this._entities.forEach((entity, idx)=>{
+            this._ctx.fillStyle = entity.color;
+            this._ctx.beginPath();
+            this._ctx.arc(idx*50+5, bottom,5, 0,2* Math.PI);
+            this._ctx.stroke();
+            this._ctx.fill();
+            this._ctx.fillStyle = "rgba(0, 0, 0, 1)";
+            this._ctx.fillText(entity.id, idx*50+13, bottom+3);
+        });
     }
 }
